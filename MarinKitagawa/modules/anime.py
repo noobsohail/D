0d9e274fc21e7f7e4e728789b9ea2c24469317b6
ꜱōʜᴇʀᴜ ᴋāɴ (AKA) ꜱᴏʜᴀɪʟ ᴋʜᴀɴ <sikhoshikaay@gmail.com>
2022-01-18 03:21:22 -0800

    d

---
+++ MarinKitagawa/modules/anime.py
@@ -0,0 +1,224 @@
+import datetime
+import html
+import textwrap
+
+import bs4
+import jikanpy
+import requests
+from telegram.utils.helpers import mention_html
+from MarinKitagawa import OWNER_ID, DRAGONS, dispatcher
+from MarinKitagawa.modules.disable import DisableAbleCommandHandler
+from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
+                      Update)
+from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
+#HEADERS
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+}
+
+info_btn = "More Information"
+kaizoku_btn = "Kaizoku ☠️"
+kayo_btn = "Kayo 🏴‍☠️"
+indi_btn = "Indi"
+prequel_btn = "⬅️ Prequel"
+sequel_btn = "Sequel ➡️"
+close_btn = "❌"
+
+
+def shorten(description, info='anilist.co'):
+    msg = ""
+    if len(description) > 700:
+        description = description[0:500] + '....'
+        msg += f"\n*Description*:\n_{description}_[Read More]({info})"
+    else:
+        msg += f"\n*Description*:\n_{description}_"
+    return msg
+
+
+#time formatter from uniborg
+def t(milliseconds: int) -> str:
+    """Inputs time in milliseconds, to get beautified time,
+    as string"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + " Days, ") if days else "") + \
+        ((str(hours) + " Hours, ") if hours else "") + \
+        ((str(minutes) + " Minutes, ") if minutes else "") + \
+        ((str(seconds) + " Seconds, ") if seconds else "") + \
+        ((str(milliseconds) + " ms, ") if milliseconds else "")
+    return tmp[:-2]
+
+
+def site_search(update: Update, context: CallbackContext, site: str):
+    message = update.effective_message
+    args = message.text.strip().split(" ", 1)
+    more_results = True
+
+    try:
+        search_query = args[1]
+    except IndexError:
+        message.reply_text("Give something to search")
+        return
+
+    if site == "kaizoku":
+        search_url = f"https://animekaizoku.com/?s={search_query}"
+        html_text = requests.get(search_url , headers=headers).text
+        soup = bs4.BeautifulSoup(html_text, "html.parser")
+        search_result = soup.find_all("h2", {'class': "post-title"})
+
+        if search_result:
+            result = f"<b>Search results for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>AnimeKaizoku</code>: \n"
+            for entry in search_result:
+                post_link = "https://animekaizoku.com/" + entry.a['href']
+                post_name = html.escape(entry.text)
+                result += f"• <a href='{post_link}'>{post_name}</a>\n"
+        else:
+            more_results = False
+            result = f"<b>No result found for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>AnimeKaizoku</code>"
+
+    elif site == "kayo":
+        search_url = f"https://kayoanime.com/?s={search_query}"
+        html_text = requests.get(search_url).text
+        soup = bs4.BeautifulSoup(html_text, "html.parser")
+        search_result = soup.find_all("h2", {'class': "post-title"})
+
+        result = f"<b>Search results for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>KayoAnime</code>: \n"
+        for entry in search_result:
+
+            if entry.text.strip() == "Nothing Found":
+                result = f"<b>No result found for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>KayoAnime</code>"
+                more_results = False
+                break
+
+            post_link = entry.a['href']
+            post_name = html.escape(entry.text.strip())
+            result += f"• <a href='{post_link}'>{post_name}</a>\n"
+           
+    elif site == "indi":
+        search_url = f"https://indianime.com/?s={search_query}"
+        html_text = requests.get(search_url , headers=headers).text
+        soup = bs4.BeautifulSoup(html_text, "html.parser")
+        search_result = soup.find_all("h2", {"class": "post-title"})
+
+        result = f"<b>Search results for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>indianime</code>: \n"
+        for entry in search_result:
+
+            if entry.text.strip() == "Nothing Found":
+                result = f"<b>No result found for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>indianime</code>"
+                more_results = False
+                break
+
+            post_link = entry.a['href']
+            post_name = html.escape(entry.text.strip())
+            result += f"• <a href='{post_link}'>{post_name}</a>\n"
+
+    elif site == "anidl":
+        search_url = f"https://anidl.org/?s={search_query}"
+        html_text = requests.get(search_url).text
+        soup = bs4.BeautifulSoup(html_text, "html.parser")
+        search_result = soup.find_all("h2", {'class': "post-title"})
+
+        result = f"<b>Search results for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>anidl</code>: \n"
+        for entry in search_result:
+
+            if entry.text.strip() == "Nothing Found":
+                result = f"<b>No result found for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>anidl</code>"
+                more_results = False
+                break
+
+            post_link = entry.a['href']
+            post_name = html.escape(entry.text.strip())
+            result += f"• <a href='{post_link}'>{post_name}</a>\n"
+
+    buttons = [[InlineKeyboardButton("See all results", url=search_url)]]
+
+    if more_results:
+        message.reply_text(
+            result,
+            parse_mode=ParseMode.HTML,
+            reply_markup=InlineKeyboardMarkup(buttons),
+            disable_web_page_preview=True)
+    else:
+        message.reply_text(
+            result, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+
+
+@run_async
+def kaizoku(update: Update, context: CallbackContext):
+    site_search(update, context, "kaizoku")
+
+
+@run_async
+def kayo(update: Update, context: CallbackContext):
+    site_search(update, context, "kayo")
+    
+@run_async
+def indi(update: Update, context: CallbackContext):
+    site_search(update, context, "indi")
+
+@run_async
+def anidl(update: Update, context: CallbackContext):
+    site_search(update, context, "anidl")    
+    
+    
+#plugin by t.me/RCage
+@run_async
+def meme(update: Update, context: CallbackContext):
+    msg = update.effective_message
+    meme = requests.get("https://meme-api.herokuapp.com/gimme/Animemes/").json()
+    image = meme.get("url")
+    caption = meme.get("title")
+    if not image:
+        msg.reply_text("No URL was received from the API!")
+        return
+    msg.reply_photo(
+                photo=image, caption=caption)
+                
+                
+                
+__help__ = """
+Get information about anime, manga or characters from [AniList](anilist.co).
+*Available commands:*
+                               
+➩ *Anime search:*                            
+ ✪ /anime <anime>*:* returns information about the anime.
+ ✪ /whatanime*:* returns source of anime when replied to photo or gif.                                                          
+ ✪ /character <character>*:* returns information about the character.
+ ✪ /manga <manga>*:* returns information about the manga.
+ ✪ /user <user>*:* returns information about a MyAnimeList user.
+ ✪ /upcoming*:* returns a list of new anime in the upcoming seasons.
+ ✪ /airing <anime>*:* returns anime airing info.
+ ✪ /indi <anime>*:* search an anime on indianime.com
+ ✪ /kaizoku <anime>*:* search an anime on animekaizoku.com
+ ✪ /kayo <anime>*:* search an anime on animekayo.com
+ ✪ /anidl <anime>*:* search an anime on anidl.org
+                               
+➩ *Watchlist:*                             
+ ✪ /watchlist*:* to get your saved watchlist.
+ ✪ /mangalist*:* to get your saved manga read list.
+ ✪ /characterlist | fcl*:* to get your favorite characters list.
+ ✪ /removewatchlist | rwl <anime>*:* to remove a anime from your list.
+ ✪ /rfcharacter | rfcl <character>*:* to remove a character from your list.  
+ ✪ /rmanga | rml <manga>*:* to remove a manga from your list.
+ 
+➩ *Anime Fun:*
+ ✪ /animequotes*:* random anime quote.
+ ✪ /meme*:* sends a random anime meme form reddit `r/animemes`.                           
+ """
+
+KAIZOKU_SEARCH_HANDLER = DisableAbleCommandHandler("kaizoku", kaizoku)
+KAYO_SEARCH_HANDLER = DisableAbleCommandHandler("kayo", kayo)
+INDI_SEARCH_HANDLER = DisableAbleCommandHandler("indi", indi)
+ANIDL_SEARCH_HANDLER = DisableAbleCommandHandler("anidl", anidl)
+MEME_HANDLER = DisableAbleCommandHandler("meme", meme)
+
+
+dispatcher.add_handler(KAIZOKU_SEARCH_HANDLER)
+dispatcher.add_handler(KAYO_SEARCH_HANDLER)
+dispatcher.add_handler(INDI_SEARCH_HANDLER)
+dispatcher.add_handler(ANIDL_SEARCH_HANDLER)
+dispatcher.add_handler(MEME_HANDLER)
+
+__mod_name__ = "Anime"
---
+++ MarinKitagawa/modules/music.py
@@ -0,0 +1,8 @@
+__mod_name__ = "Music"
+
+__help__ = """
+Erza Music System
+To download music use /song
+To download video use /video
+To play video from youtube use /play for audio \n or /vplay for video stream
+If your group is private add @erzamusica"""
